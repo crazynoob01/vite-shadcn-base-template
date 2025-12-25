86284d926b75d15713bd6f565a60fa17fcc40922
edwardchan <edward@newswav.com>
2025-12-26 02:43:17 +0800

    lighten the base template

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import './App.css'
-import { Toaster } from '@/components/ui/toaster'
 import { Toaster as Sonner } from '@/components/ui/sonner'
 import { TooltipProvider } from '@/components/ui/tooltip'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
@@ -11,7 +10,6 @@ const queryClient = new QueryClient()
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster />
       <Sonner />
       <BrowserRouter>
         <Routes>
